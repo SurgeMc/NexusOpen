2a0b7bbcd68a634468dcd36218341836bf1d629a
omg123 <tharmsy@transgender.army>
2024-12-14 02:46:08 +0000

    1

--- src/main/java/dev/stephen/nexus/Main.java
+++ src/main/java/dev/stephen/nexus/Main.java
@@ -10,6 +10,6 @@ public final class Main implements ModInitializer {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        System.out.println("Loading Nexus...");
+        System.out.println("Loading Cubzyn...");
     }
 }
